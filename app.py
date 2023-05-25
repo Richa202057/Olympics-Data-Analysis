d5e15b860cd7a02811b322af2d4d0408f1ddaf71
Richa Kumari <106690186+Richa202057@users.noreply.github.com>
2023-05-26 00:13:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import preprocessor,helper
+import preprocessing,statistical_analysis
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -9,7 +9,7 @@ import plotly.figure_factory as ff
 df = pd.read_csv('athlete_events.csv')
 region_df = pd.read_csv('noc_regions.csv')
 
-df = preprocessor.preprocess(df,region_df)
+df = preprocessing.preprocess(df,region_df)
 
 st.sidebar.title("Olympics Analysis")
 st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
@@ -20,12 +20,12 @@ user_menu = st.sidebar.radio(
 
 if user_menu == 'Medal Tally':
     st.sidebar.header("Medal Tally")
-    years,country = helper.country_year_list(df)
+    years,country = statistical_analysis.country_year_list(df)
 
     selected_year = st.sidebar.selectbox("Select Year",years)
     selected_country = st.sidebar.selectbox("Select Country", country)
 
-    medal_tally = helper.fetch_medal_tally(df,selected_year,selected_country)
+    medal_tally = statistical_analysis.fetch_medal_tally(df,selected_year,selected_country)
     if selected_year == 'Overall' and selected_country == 'Overall':
         st.title("Overall Tally")
     if selected_year != 'Overall' and selected_country == 'Overall':
