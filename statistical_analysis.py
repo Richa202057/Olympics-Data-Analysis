854a138e36e5abc47ff8f156e8bc1861264ab86d
Richa Kumari <106690186+Richa202057@users.noreply.github.com>
2023-06-10 17:20:24 +0530

    Update statistical_analysis.py

--- statistical_analysis.py
+++ statistical_analysis.py
@@ -41,12 +41,10 @@ def country_year_list(df):
 
     return years,country
 
-def data_over_time(df,col):
-
-    nations_over_time = df.drop_duplicates(['Year',col])['Year'].value_counts().reset_index().sort_values('index')
-    nations_over_time.rename(columns={'index': 'Edition', 'Year': col}, inplace=True)
+def data_over_time(df, col):
+    nations_over_time = df.drop_duplicates(['Year', col])['Year'].value_counts().reset_index().sort_values('Year')
+    nations_over_time.rename(columns={'Year': 'Edition', 'count': col}, inplace=True)
     return nations_over_time
-
 
 def most_successful(df, sport):
     temp_df = df.dropna(subset=['Medal'])
