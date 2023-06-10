b4ac2ac9af1b68798c3d2fbf500b1c3a2d1b6247
Richa Kumari <106690186+Richa202057@users.noreply.github.com>
2023-06-10 18:27:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ region_df = pd.read_csv('noc_regions.csv')
 
 df = preprocessing.preprocess(df,region_df)
 
-st.title("""red[Olympics Data Analysis]""")
+st.title(""":red[Olympics Data Analysis]""")
 st.sidebar.image('olympics_pic2.jpg')
 user_menu = st.sidebar.radio(
     'Select an Option',
